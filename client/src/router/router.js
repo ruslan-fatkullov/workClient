ca966a75773d3766579ae2a5d44bf4029c9ed674
ruslan-fatkullov <fatkullov1999@gmail.com>
2023-06-20 13:48:11 +0400

    new com1

--- client/src/router/router.js
+++ client/src/router/router.js
@@ -1,5 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter } from 'vue-router'
 
 const routes = [
   {
@@ -51,7 +50,6 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
   routes
 })
 
